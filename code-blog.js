4496607c028069781ff7096d16927235174eb0a7
Rikki Hansen <rikkihansen@Rikkis-MacBook-Air.local>
2016-05-08 19:18:51 -0700

    fixed some spelling

--- code-blog.js
+++ code-blog.js
@@ -8,19 +8,28 @@ function Article (opts) {
   this.contributers = opts.contributers;
   this.body = opts.body;
   this.contributersUrl = opts.contributersUrl;
-  this.catagory = opts.catagory;
+  this.category = opts.category;
 }
 
 Article.prototype.toHtml= function () {
   var $newArticle = $('article.template').clone();
-  $newArticle.attr('data-catagory', this.catagory);
+  $newArticle.attr('data-category', this.category);
   $newArticle.find('h1').text(this.title);
-  $newArticle.find("address > a").html(this.contributers);
-  $newArticle.find("address > a").attr('href', this.contributersUrl);
-  $(".article-body").html(this.body);
-  $newArticle.append('<hr>');
+  $newArticle.find('address > a').html(this.contributers);
+  $newArticle.find('address > a').attr('href', this.contributersUrl);
+  $('.article-body').html(this.body);
+  // $newArticle.append('<hr>');
   $newArticle.removeClass('template');
+
 return $newArticle;
 }
-
+//
+// data.forEach(function(ele) {
+//   articles.push(new Article(ele));
+// })
+//
+// articles.forEach(function(a){
+//   $('#articles').append(a.toHtml())
+// });
+//
 console.log("foo");
